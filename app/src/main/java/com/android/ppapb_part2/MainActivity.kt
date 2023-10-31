15095b0e8b933d0de77564c5b98946b8fd44fbf9
Whyaziz <rizkyaziz214@gmail.com>
2023-11-01 02:45:34 +0700

    fix: mengosongkan project

--- app/src/main/java/com/android/ppapb_part2/MainActivity.kt
+++ app/src/main/java/com/android/ppapb_part2/MainActivity.kt
@@ -1,34 +1,11 @@
 package com.android.ppapb_part2
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.viewpager2.widget.ViewPager2
-import com.android.ppapb_part2.databinding.ActivityMainBinding
-import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-    lateinit var mediator: TabLayoutMediator
-    lateinit var viewPager2: ViewPager2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        with(binding){
-            viewPager2 = viewPager
-            viewPager.adapter = TabAdapter(supportFragmentManager,this@MainActivity.lifecycle)
-            mediator = TabLayoutMediator(tabLayout,viewPager)
-            {
-                    tab,position -> when(position){
-                0 -> tab.text = "Login"
-                1 -> tab.text = "Register"
-            }
-            }
-            mediator.attach()
-
-        }
+        setContentView(R.layout.activity_main)
     }
 }
