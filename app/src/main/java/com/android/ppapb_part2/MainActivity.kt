9d69e27076d781b4a594d1d6d36ae8fefeaeb258
Whyaziz <rizkyaziz214@gmail.com>
2023-11-02 08:25:31 +0700

    feat : add feature full

--- app/src/main/java/com/android/ppapb_part2/MainActivity.kt
+++ app/src/main/java/com/android/ppapb_part2/MainActivity.kt
@@ -2,10 +2,22 @@ package com.android.ppapb_part2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.android.ppapb_part2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        title = "Travel App"
+
+        with(binding){
+            val navController = findNavController(R.id.nav_host_fragment)
+            bottomNavigationView.setupWithNavController(navController)
+        }
     }
 }
---
+++ app/src/main/java/com/android/ppapb_part2/SharedViewModel.kt
@@ -0,0 +1,7 @@
+package com.android.ppapb_part2
+
+import androidx.lifecycle.ViewModel
+
+class SharedViewModel : ViewModel() {
+    var selectedTicketType: String = "Ticket Type"
+}
